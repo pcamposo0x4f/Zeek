7a45b33d1d3bcb17c3dbc64d4da0d4174cf34a49
Patricio Campos <38844915+pcamposo0x4f@users.noreply.github.com>
2019-10-01 13:23:32 -0300

    Actualizacion

--- zeek_installer_script.sh
+++ zeek_installer_script.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
-## Instalacion de Repositorios y Claves
-sudo wget -nv http://download.opensuse.org/repositories/network:bro/xUbuntu_18.04/Release.key -O Release.key
-sudo apt-key add - < Release.key
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/bro.list"
-## Upgrade && Update
+## Instalacion de Requisitos
 sudo apt update
 sudo apt upgrade -y
-sudo apt autoremove
+sudo apt-get install libmaxminddb-dev cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev gdb -y
 
-## Instalacion Bro
-sudo apt install bro -y
-sudo apt autoremove -y
+## Descarga de los fuentes
+sudo git clone --recursive https://github.com/zeek/zeek
 
-## Instalacion de Splunk
-sudo wget -O splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.1.1&product=splunk&filename=splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb&wget=true'
-sudo dpkg -i splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
-
-## Instalacion de GeoIP
-sudo apt-get install libmaxminddb-dev
+## Descarga de Base de Datos de GeoIP
 sudo wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
-sudo tar zxf GeoLite2-City.tar.gz
 sudo mv GeoLite2-City_20190924/GeoLite2-City.mmdb /usr/share/GeoIP/
+
+## Instalacion Zeek
+
+cd zeek/
+sudo ./configure --with-geoip=/usr/share/GeoIP/GeoLite2-City.mmdb
+sudo make
+sudo make install
